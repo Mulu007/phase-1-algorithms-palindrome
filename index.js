25c71caf6653057070640d4c86c930adb3384d52
Mulu <brianmulu007@gmail.com>
2022-08-25 15:10:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let len = word.length;
+  let mid = Math.floor(len/2)
+
+  for (let i = 0; i < mid; i++) { 
+    if (word[i] !== word[len - 1 -i]) {
+      return false;
+    }
+  }
+  return true;
 }
+
+// }
+
 
 /* 
   Add your pseudocode here
+  start
+  if the letters of the split word match match retun true 
+     else return false
+  stop
 */
 
 /*
   Add written explanation of your solution here
+  iteration starts from the first letter of the word to the middle letter splitting the letter into two.
+  The code then checks each letter startinf from the beginning letter with the last letter.
+  If the letters match then the code return true.
+  If the letters do not match the code return false. 
 */
 
 // You can run `node index.js` to view these console logs
